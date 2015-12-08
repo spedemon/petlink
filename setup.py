6f83c69efc3570624cd7af15606d5aad2f015918
Stefano Pedemonte <stefano.pedemonte@gmail.com>
2015-12-08 18:13:38 -0500

    Suport streaming of petlink32

---
+++ setup.py
@@ -0,0 +1,47 @@
+
+# petlink - Decode and encode PETlink streams. 
+# Stefano Pedemonte
+# Aalto University, School of Science, Helsinki
+# Oct 2013, Helsinki 
+# Jan 2015, Helsinki
+
+
+# Use old Python build system, otherwise the extension libraries cannot be found. FIXME 
+import sys
+for arg in sys.argv: 
+    if arg=="install":
+        sys.argv.append('--old-and-unmanageable') 
+
+from setuptools import setup, Extension
+from glob import glob 
+
+petlink32_c_module = Extension('petlink.petlink32_c', ['petlink/petlink32_c.c']) 
+
+setup(
+    name='petlink',
+    version='0.3.0',
+    author='Stefano Pedemonte',
+    author_email='stefano.pedemonte@gmail.com',
+    packages=['petlink', 'petlink.examples', 'petlink.tests'], 
+    ext_modules=[petlink32_c_module, ],
+    test_suite = "petlink.tests", 
+    url='http://www.occiput.io/',
+    license='LICENSE.txt',
+    description='Decode and encode PETlink streams.',
+    long_description=open('README.rst').read(),
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+    install_requires=[
+        "numpy >= 1.6.0", 
+        "simplewrap >= 0.3.0", 
+    ], 
+)
+
